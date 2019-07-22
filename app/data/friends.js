58f5a80114ba34ccb02ff7b6c7d7dc2c021007a8
g-uh22 <gialeerobbins@gmail.com>
2019-07-21 19:24:09 -0700

    images matches and arrays corrected

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,37 @@
-var friends = [{
-        "name": "Erisdar The Mysterious One",
-        "photo": "https://forum.unity3d.com/proxy.php?image=http%3A%2F%2Fwww.infinitypbr.com%2Fdragon%2Fhd6.jpg&hash=572c8a1450b0670344e0da08cae18d18",
-        "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+const friends = [{
+        "name": "Harmony Korine",
+        "photo": "https://media.gq.com/photos/5c7ff9b9d985d43ce1e03d20/master/w_1600%2Cc_limit/harmony-korine-gq-april-2019-01.jpg",
+        "scores": [4, 5, 1, 2, 5, 1, 4, 3, 4, 3]
     },
     {
-        "name": "Mistical Taytay",
-        "photo": "http://rizalprovince.ph/tourism_pictures/Avilon%20MZP%20-%20Montalban%20(2).jpg",
-        "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+        "name": "Lisa Vanderpump",
+        "photo": "https://www.bravotv.com/sites/bravo/files/field_person_full_photo/2018/12/lisa-vanderpump.png",
+        "scores": [3, 2, 5, 5, 5, 1, 4, 4, 3, 5]
     },
     {
-        "name": "Skylar Rose",
-        "photo": "http://i2.wp.com/northstarz.ca/wp-content/uploads/2013/12/Rose.jpg",
-        "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+        "name": "Kylie Jenner",
+        "photo": "http://www.gstatic.com/tv/thumb/persons/532961/532961_v9_bc.jpg",
+        "scores": [5, 4, 5, 5, 1, 1, 2, 3, 4, 1]
     },
     {
-        "name": "Colonel Paul",
-        "photo": "http://www.153aw.ang.af.mil/shared/media/bio/Colonel%20Paul%20Lyman.jpg",
-        "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+        "name": "Tommy Lee",
+        "photo": "https://img1.nickiswift.com/img/gallery/the-shady-side-of-tommy-lee/intro-1520523645.jpg",
+        "scores": [4, 5, 2, 2, 5, 1, 5, 1, 4, 5]
     },
     {
-        "name": "Edward Snowden",
-        "photo": "https://upload.wikimedia.org/wikipedia/commons/6/60/Edward_Snowden-2.jpg",
-        "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+        "name": "Julian Assange",
+        "photo": "http://www.gstatic.com/tv/thumb/persons/670142/670142_v9_ba.jpg",
+        "scores": [5, 3, 2, 1, 5, 2, 1, 5, 5, 1]
     },
     {
-        "name": "Hesso Nader",
-        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Aatef_Jenyat.jpg/293px-Aatef_Jenyat.jpg",
-        "scores": [3, 3, 4, 1, 4, 3, 5, 3, 5, 4]
+        "name": "Beyonce",
+        "photo": "https://cdn.cnn.com/cnnnext/dam/assets/180415033653-01-beyonce-coachella-0415-exlarge-169.jpg",
+        "scores": [4, 5, 3, 2, 3, 5, 2, 1, 5, 2]
     },
     {
-        "name": "Sonneth The Swift",
-        "photo": "http://www.billboard.com/files/styles/article_main_image/public/stylus/1235529-taylor-swift-woman-of-the-year-617-409.jpg",
-        "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+        "name": "Vince Vaughn",
+        "photo": "https://m.media-amazon.com/images/M/MV5BMTQ1MDA0MzE0MF5BMl5BanBnXkFtZTYwOTUzOTI4._V1_UY317_CR7,0,214,317_AL_.jpg",
+        "scores": [4, 3, 5, 2, 4, 1, 5, 2, 4, 5]
     }
 
 ]
